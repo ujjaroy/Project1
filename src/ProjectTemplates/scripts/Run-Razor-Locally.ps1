297a054babe93049ff52337c36449a0355a792ad
yourname <yourname@domain.com>
2023-06-12 09:32:08 +0530

    code change

--- src/ProjectTemplates/scripts/Run-Razor-Locally.ps1
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env powershell
-#requires -version 4
-
-[CmdletBinding(PositionalBinding = $false)]
-param()
-
-. $PSScriptRoot\Test-Template.ps1
-
-Test-Template "webapp" "webapp -au Individual" "Microsoft.DotNet.Web.ProjectTemplates.8.0.8.0.0-dev.nupkg" $false
